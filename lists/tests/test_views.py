fef884f17e75c446dfce5e64451cc8fd261a586b
Ben Goodstein <ben.goodstein@it.ox.ac.uk>
2016-07-01 13:32:47 +0100

    new existing list item form and tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,6 +7,7 @@ from django.utils.html import escape
 from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from unittest import skip
 
 class HomePageTest(TestCase):
     def test_home_page_renders_home_template(self):
@@ -16,6 +17,7 @@ class HomePageTest(TestCase):
     def test_home_page_uses_item_form(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class NewListTest(TestCase):
 
@@ -120,6 +122,7 @@ class ListViewTest(TestCase):
             data={'text': ''}
         )
 
+
     def test_for_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
         self.assertEqual(Item.objects.count(), 0)
@@ -129,13 +132,29 @@ class ListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
 
+
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
         self.assertIsInstance(response.context['form'], ItemForm)
 
+
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
