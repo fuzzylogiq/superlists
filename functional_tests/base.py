bc4ac95ad7ec8dd09feb08c87a460478b5ba09de
Ben Goodstein <ben.goodstein@it.ox.ac.uk>
2016-07-01 12:06:13 +0100

    use form in all views, back to working state

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -31,4 +31,4 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def get_item_input_box(self):
-        return self.browser.find_element_by_id('text')
+        return self.browser.find_element_by_id('id_text')
